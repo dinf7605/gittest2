d70506ecf9c89812cb04ab5843f87df1d008d47c
knuser <knuser@DESKTOP-1EFUYTC>
2025-12-30 15:33:20 +0900

    전땅크가 나가신다 길을 비켜라!!!

---
+++ gittest2/src/com/git/main/JeonTank1948.java
@@ -0,0 +1,10 @@
+package com.git.main;
+
+public class JeonTank1948 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("전땅크가 나가신다 길을비켜라!!!");
+	}
+
+}
